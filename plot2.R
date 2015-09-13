b389e9baac6e49cbda330d221598d3ac6b30caa8
nhchi <chi.nguyen220609@gmail.com>
2015-09-13 21:45:00 +0700

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,5 +1,5 @@
 ## Getting full dataset
-# raw data (.txt) files are downloaded and saved to 'path_to_working_folder/ExData_Plotting1/Data/'
+# raw data (.txt) files are saved to 'path_to_working_folder/ExData_Plotting1/Data/'
 dataset_raw <- read.csv("./ExData_Plotting1/Data/household_power_consumption.txt", header=T, sep=';', na.strings="?", 
                       nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
 dataset_raw$Date <- as.Date(dataset_raw$Date, format="%d/%m/%Y")
